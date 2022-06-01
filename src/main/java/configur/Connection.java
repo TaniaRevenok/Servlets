3b4fd1b4c81b3edfc1e24634af1ad9c6441639f2
TR <89931570+TaniaRevenok@users.noreply.github.com>
2022-06-01 11:44:23 +0300

    Create Connection.java

---
+++ src/main/java/configur/Connection.java
@@ -0,0 +1,48 @@
+package configur;
+
+import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Connection{
+    private Properties properties = null;
+
+    public Connection(ServletContext context) {
+        load(context);
+    }
+
+    public String getHostname() {
+        return properties.getProperty("database.hostname");
+    }
+
+    public String getSchema() {
+        return properties.getProperty("database.schema");
+    }
+
+    public Integer getPort() {
+        return Integer.parseInt(properties.getProperty("database.port"));
+    }
+
+    public String getUser() {
+        return properties.getProperty("database.user");
+    }
+
+    public String getPassword() {
+        return properties.getProperty("database.password");
+    }
+
+    public String getJdbcDriver() {
+        return properties.getProperty("jdbc.driver");
+    }
+
+    private void load(ServletContext context) {
+        this.properties = new Properties();
+        try (InputStream is = context.getResourceAsStream("/WEB-INF/resources/application.properties")) {
+            properties.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
